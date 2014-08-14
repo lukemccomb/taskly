d3359b27ba1e03727d60fa9809b1999f485d5132
Luke McComb <lucasmccomb@gmail.com>
2014-08-14 16:44:38 -0600

    javascript enabled to hide and show tasks underneath task lists

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,13 @@
 //= require jquery_ujs
 //= require_tree .
 
-
+$(document).ready(function() {
+//signature hello world
+  console.log('Smells like updog in here');
+//Declaring global var
+//login form
+  $('.arrow').on('click', function() {
+    var mom = $(this).parents('.task-list');
+    mom.find('.task').toggle();
+  });
+});
