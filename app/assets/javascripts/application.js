fed09c01959a70bd85470a3d01d4fe587a10baa8
Luke McComb <lucasmccomb@gmail.com>
2014-08-15 14:39:29 -0600

    open tasks close when opening another task list, arrows follow visibility of tasklists with direction

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,10 +5,20 @@
 $(document).ready(function() {
 //signature hello world
   console.log('Smells like updog in here');
-//Declaring global var
-//login form
+//hiding tasks
+  $('.task').hide();
+//showing & hiding tasks
   $('.arrow').on('click', function() {
     var mom = $(this).parents('.task-list');
-    mom.find('.task').toggle();
+    $('.task').hide();
+    $('.arrow').removeClass('arrow_rotate');
+    var curr_task = mom.find('.task');
+    if (curr_task.not(':visible')) {
+     curr_task.slideDown();
+     mom.find('.arrow').toggleClass('arrow_rotate');
+    } else if (curr_task.is(':visible')) {
+      curr_task.slideUp();
+      mom.find('.arrow').toggleClass('arrow_rotate');
+    }
   });
 });
